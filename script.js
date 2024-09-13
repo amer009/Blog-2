a842982959da45c92157eea3ecf94e25dcfd1972
amer009 <amer00966@gmail.com>
2024-09-13 11:33:13 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const enviar= document.getElementById("enviar")
+enviar.addEventListener('click',()=>{
+
+    const nombre= document.getElementById("nombre")
+    const nombret= nombre.value
+    alert("Bienvenido "+  nombret  + "Registro exitoso")
+
+
+})
+ 
